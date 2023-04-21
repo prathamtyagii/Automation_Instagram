ac449bf4b088f07d2ef5f0c2438e3bd2c8654a00
prathamtyagii <78618628+prathamtyagii@users.noreply.github.com>
2023-04-22 05:16:07 +0530

    Update Automation_insta.js

--- Automation_insta.js
+++ Automation_insta.js
@@ -2,7 +2,7 @@
 let minimist=require('minimist');
 let puppeteer=require('puppeteer');
 let fs=require('fs');
-let robot=require('robotjs');
+//let robot=require('robotjs');
 
 let args=minimist(process.argv);
 let configJSON = fs.readFileSync(args.config, "utf-8");
@@ -37,38 +37,54 @@ async function run() {
     //Following a public account
     await page.waitFor(3000);
     await page.keyboard.press("Tab");
-    await page.keyboard.press("Tab");
 
     await page.keyboard.press("Enter");
 
     await page.waitFor(3000);
-    await page.keyboard.press("Tab");
+    await page.waitForSelector('svg._ab6-');
+    await page.click('svg._ab6-');
     await page.keyboard.press("Tab");
 
     await page.keyboard.press("Enter");
 
-    await page.waitForSelector('input.XTCLo');
-    await page.type('input.XTCLo',config.toBeFollowed[0],{delay:200});
+    await page.waitForSelector('input._aauy');
+    await page.type('input._aauy',config.toBeFollowed[0],{delay:200});
 
     await page.waitFor(3000);
 
     await page.keyboard.press("Enter");
     await page.keyboard.press("Enter");
 
-    await page.waitForSelector('button._5f5mN.jIbKX._6VtSN');
-    await page.click('button._5f5mN.jIbKX._6VtSN');
+    await page.waitFor(3000);
+
+    await page.keyboard.press("Tab");
+
+    await page.waitFor(3000);
+
+    await page.keyboard.press("Enter");
 
     await page.waitFor(3000);
     
     //Clicking the home button
 
-    await page.goto(args.url);
+    await page.waitForSelector('svg._ab6-');
+    await page.click('svg._ab6-');
 
+    await page.waitFor(3000);
+
+    await page.keyboard.press("Enter");
     await page.waitFor(3000);
 
     //Sending Follow request to a private account
-    await page.waitForSelector('input.XTCLo');
-    await page.type('input.XTCLo',config.toBeFollowed[1],{delay:200});
+
+    await page.waitForSelector('svg._ab6-');
+    await page.click('svg._ab6-');
+    await page.keyboard.press("Tab");
+
+    await page.keyboard.press("Enter");
+
+    await page.waitForSelector('input._aauy');
+    await page.type('input._aauy',config.toBeFollowed[1],{delay:200});
 
     await page.waitFor(3000);
 
@@ -77,48 +93,83 @@ async function run() {
 
     await page.waitFor(3000);
 
-    await page.waitForSelector('button.sqdOP');
-    await page.click('button.sqdOP');
+    await page.keyboard.press("Tab");
 
     await page.waitFor(3000);
 
-    //Clicking the home button 
+    await page.keyboard.press("Enter");
 
-    await page.goto(args.url);
+    await page.waitFor(3000);
+    
+    //Clicking the home button
 
+    await page.waitForSelector('svg._ab6-');
+    await page.click('svg._ab6-');
+
+    await page.waitFor(3000);
+
+    await page.keyboard.press("Enter");
     await page.waitFor(3000);
 
     //Texting a follower
-    await page.waitForSelector('input.XTCLo');
-    await page.type('input.XTCLo',config.followers[0],{delay:200});
+
+    await page.waitForSelector('svg._ab6-');
+    await page.click('svg._ab6-');
+    await page.keyboard.press("Tab");
+
+    await page.keyboard.press("Enter");
+
+    await page.waitForSelector('input._aauy');
+    await page.type('input._aauy',config.followers[1],{delay:200});
 
     await page.waitFor(3000);
 
     await page.keyboard.press("Enter");
     await page.keyboard.press("Enter");
 
-    await page.waitForSelector('button.sqdOP.L3NKy._8A5w5');
-    await page.click('button.sqdOP.L3NKy._8A5w5');
-
     await page.waitFor(3000);
 
     await page.keyboard.press("Tab");
     await page.keyboard.press("Tab");
+
+    await page.waitFor(3000);
+
     await page.keyboard.press("Enter");
 
-    await page.waitForSelector('div.qF0y9>textarea');
-    await page.type('div.qF0y9>textarea',config.message,{delay:200});
+    await page.waitFor(3000);
 
-    await page.keyboard.press("Enter");   
+    await page.waitForSelector('.');
+    await page.type('.',config.message,{delay:200});
+
+    await page.keyboard.press("H");
+    await page.keyboard.press("i");
+    await page.keyboard.press("i");
+
+    await page.keyboard.press("Enter");  
+    await page.waitFor(3000); 
     
     //Clicking the home button
-    await page.goto(args.url);
+    await page.waitForSelector('svg._ab6-');
+    await page.click('svg._ab6-');
 
+    await page.waitFor(3000);
+
+    await page.keyboard.press("Enter");
     await page.waitFor(3000);
 
     //Unfollowing a private account
-    await page.waitForSelector('input.XTCLo');
-    await page.type('input.XTCLo',config.toBeFollowed[0],{delay:200});
+
+    await page.keyboard.press("Enter");
+
+    await page.waitFor(3000);
+    await page.waitForSelector('svg._ab6-');
+    await page.click('svg._ab6-');
+    await page.keyboard.press("Tab");
+
+    await page.keyboard.press("Enter");
+
+    await page.waitForSelector('input._aauy');
+    await page.type('input._aauy',config.followers[0],{delay:200});
 
     await page.waitFor(3000);
 
@@ -127,71 +178,46 @@ async function run() {
 
     await page.waitFor(3000);
 
-    await page.waitForSelector('button._5f5mN.-fzfL._6VtSN');
-    await page.click('button._5f5mN.-fzfL._6VtSN');
-
-    await page.waitFor(3000);
-
-    await page.waitForSelector('button.aOOlW.-Cab_');
-    await page.click('button.aOOlW.-Cab_');
-
-    await page.waitFor(3000);
-
-    //Posting a photo
-    await page.waitFor(6000);
-
-    await page.waitForSelector('div.vZuFV>button.wpO6b.ZQScA');
-    await page.click('div.vZuFV>button.wpO6b.ZQScA');
-
-    await page.waitFor(3000);
-
     await page.keyboard.press("Tab");
-    await page.keyboard.press("Tab");
+
+    await page.waitFor(3000);
+
     await page.keyboard.press("Enter");
 
-    await page.waitFor(10000);
-
-    robot.moveMouse(1300, 500);
-    /*robot.mouseClick('left');
-
-    robot.keyTap("right");
-    
-    robot.keyTap("enter");
-
-    robot.keyTap("right");
-    
-    robot.keyTap("right");
-
-    robot.keyTap("enter");*/
-
-    await page.waitForSelector('body > div.RnEpo.gpWnf.Yx5HN > div.pbNvD > div > div > div > div.qF0y9.Igw0E.IwRSH.eGOV_._4EzTm > div > div > div:nth-child(3) > div > button');
-    await page.click('body > div.RnEpo.gpWnf.Yx5HN > div.pbNvD > div > div > div > div.qF0y9.Igw0E.IwRSH.eGOV_._4EzTm > div > div > div:nth-child(3) > div > button');    
-    
-    await page.waitForSelector('body > div.RnEpo.gpWnf.Yx5HN > div.pbNvD > div > div > div > div.qF0y9.Igw0E.IwRSH.eGOV_._4EzTm > div > div > div:nth-child(3) > div > button');
-    await page.click('body > div.RnEpo.gpWnf.Yx5HN > div.pbNvD > div > div > div > div.qF0y9.Igw0E.IwRSH.eGOV_._4EzTm > div > div > div:nth-child(3) > div > button');
-
-    await page.waitForSelector('textarea.lFzco');
-    await page.type('textarea.lFzco','This is the Caption to our post',{delay:100});
+    await page.waitFor(3000);
 
     await page.keyboard.press("Tab");
     await page.keyboard.press("Tab");
     await page.keyboard.press("Tab");
     await page.keyboard.press("Tab");
     await page.keyboard.press("Tab");
-    await page.keyboard.press("Tab");
-    await page.keyboard.press("Tab");
-    await page.keyboard.press("Tab");
+
     await page.keyboard.press("Enter");
+
     await page.waitFor(3000);
 
     //Clicking the home button 
 
-    await page.goto(args.url);
+    await page.waitForSelector('svg._ab6-');
+    await page.click('svg._ab6-');
+
+    await page.waitFor(3000);
+
+    await page.keyboard.press("Enter");
     await page.waitFor(3000);
 
     //Liking all posts of a person
-    await page.waitForSelector('input.XTCLo');
-    await page.type('input.XTCLo',config.followers[0],{delay:200});
+    await page.keyboard.press("Enter");
+
+    await page.waitFor(3000);
+    await page.waitForSelector('svg._ab6-');
+    await page.click('svg._ab6-');
+    await page.keyboard.press("Tab");
+
+    await page.keyboard.press("Enter");
+
+    await page.waitForSelector('input._aauy');
+    await page.type('input._aauy',config.followers[1],{delay:200});
 
     await page.waitFor(3000);
 
@@ -200,6 +226,7 @@ async function run() {
 
     await page.waitFor(3000);
 
+    await page.keyboard.press("Tab");
     await page.keyboard.press("Tab");
     await page.keyboard.press("Tab");
     await page.keyboard.press("Tab");
@@ -215,21 +242,87 @@ async function run() {
     let val=true;
     
     while(val){
-        await page.waitForSelector('span.fr66n>button.wpO6b');
-        await page.click('span.fr66n>button.wpO6b');
+        await page.waitForSelector('span._aamw>button._abl-');
+        await page.click('span._aamw>button._abl-');
 
         await page.waitFor(3000);
 
         try {
-            let waitforselector= await page.waitForSelector('div.l8mY4.feth3>button.wpO6b', {visible: true, timeout: 1000 })
-            await page.click('div.l8mY4.feth3>button.wpO6b');
+            let waitforselector= await page.waitForSelector('div._aaqg>button._abl-', {visible: true, timeout: 1000 })
+            await page.click('div._aaqg>button._abl-');
         } catch (e) {
             val=false;
-            await page.waitForSelector('div.qF0y9.Igw0E.IwRSH>button.wpO6b');
-            await page.click('div.qF0y9.Igw0E.IwRSH>button.wpO6b');
+            await page.waitForSelector('svg.x1lliihq.x1n2onr6');
+            await page.click('svg.x1lliihq.x1n2onr6');
         }
 
     }
+
+    //Clicking the home button
+    await page.waitForSelector('svg._ab6-');
+    await page.click('svg._ab6-');
+
+    await page.waitFor(3000);
+
+    await page.keyboard.press("Enter");
+    await page.waitFor(3000);
+
+    //Posting a photo
+    await page.waitFor(3000);
+    await page.waitForSelector('svg._ab6-');
+    await page.click('svg._ab6-');
+
+    await page.waitFor(3000);
+
+    await page.keyboard.press("Tab");
+    await page.keyboard.press("Tab");
+    await page.keyboard.press("Tab");
+    await page.keyboard.press("Tab");
+    await page.keyboard.press("Tab");
+    await page.keyboard.press("Tab");
+
+    await page.keyboard.press("Enter");
+
+    await page.waitFor(3000);
+
+    await page.keyboard.press("Tab");
+
+    await page.keyboard.press("Enter");
+
+    await page.waitFor(10000);
+
+    //robot.moveMouse(1300, 500);
+    //robot.mouseClick('left');
+
+    //robot.keyTap("right");
+    
+    //robot.keyTap("enter");
+
+    //robot.keyTap("right");
+    
+    //robot.keyTap("right");
+
+    //robot.keyTap("enter");
+    
+
+    await page.waitForSelector('#mount_0_0_LO > div > div > div:nth-child(3) > div > div > div.x9f619.x1n2onr6.x1ja2u2z > div > div.x1uvtmcs.x4k7w5x.x1h91t0o.x1beo9mf.xaigb6o.x12ejxvf.x3igimt.xarpa2k.xedcshv.x1lytzrv.x1t2pt76.x7ja8zs.x1n2onr6.x1qrby5j.x1jfb8zj > div > div > div > div > div.x7r02ix.xf1ldfh.x131esax.xdajt7p.xxfnqb6.xb88tzc.xw2csxc.x1odjw0f.x5fp0pe > div > div > div > div.x9f619.xjbqb8w.x78zum5.x168nmei.x13lgxp2.x5pf9jr.xo71vjh.x1n2onr6.x1plvlek.xryxfnj.x1c4vz4f.x2lah0s.xdt5ytf.xqjyukv.x1qjc9v5.x1oa3qoh.x1nhvcw1 > div > div > div._ac7b._ac7d > div > div');
+    await page.click('#mount_0_0_LO > div > div > div:nth-child(3) > div > div > div.x9f619.x1n2onr6.x1ja2u2z > div > div.x1uvtmcs.x4k7w5x.x1h91t0o.x1beo9mf.xaigb6o.x12ejxvf.x3igimt.xarpa2k.xedcshv.x1lytzrv.x1t2pt76.x7ja8zs.x1n2onr6.x1qrby5j.x1jfb8zj > div > div > div > div > div.x7r02ix.xf1ldfh.x131esax.xdajt7p.xxfnqb6.xb88tzc.xw2csxc.x1odjw0f.x5fp0pe > div > div > div > div.x9f619.xjbqb8w.x78zum5.x168nmei.x13lgxp2.x5pf9jr.xo71vjh.x1n2onr6.x1plvlek.xryxfnj.x1c4vz4f.x2lah0s.xdt5ytf.xqjyukv.x1qjc9v5.x1oa3qoh.x1nhvcw1 > div > div > div._ac7b._ac7d > div > div');    
+    
+    await page.waitForSelector('#mount_0_0_LO > div > div > div:nth-child(3) > div > div > div.x9f619.x1n2onr6.x1ja2u2z > div > div.x1uvtmcs.x4k7w5x.x1h91t0o.x1beo9mf.xaigb6o.x12ejxvf.x3igimt.xarpa2k.xedcshv.x1lytzrv.x1t2pt76.x7ja8zs.x1n2onr6.x1qrby5j.x1jfb8zj > div > div > div > div > div.x7r02ix.xf1ldfh.x131esax.xdajt7p.xxfnqb6.xb88tzc.xw2csxc.x1odjw0f.x5fp0pe > div > div > div > div.x9f619.xjbqb8w.x78zum5.x168nmei.x13lgxp2.x5pf9jr.xo71vjh.x1n2onr6.x1plvlek.xryxfnj.x1c4vz4f.x2lah0s.xdt5ytf.xqjyukv.x1qjc9v5.x1oa3qoh.x1nhvcw1 > div > div > div._ac7b._ac7d > div > div');
+    await page.click('#mount_0_0_LO > div > div > div:nth-child(3) > div > div > div.x9f619.x1n2onr6.x1ja2u2z > div > div.x1uvtmcs.x4k7w5x.x1h91t0o.x1beo9mf.xaigb6o.x12ejxvf.x3igimt.xarpa2k.xedcshv.x1lytzrv.x1t2pt76.x7ja8zs.x1n2onr6.x1qrby5j.x1jfb8zj > div > div > div > div > div.x7r02ix.xf1ldfh.x131esax.xdajt7p.xxfnqb6.xb88tzc.xw2csxc.x1odjw0f.x5fp0pe > div > div > div > div.x9f619.xjbqb8w.x78zum5.x168nmei.x13lgxp2.x5pf9jr.xo71vjh.x1n2onr6.x1plvlek.xryxfnj.x1c4vz4f.x2lah0s.xdt5ytf.xqjyukv.x1qjc9v5.x1oa3qoh.x1nhvcw1 > div > div > div._ac7b._ac7d > div > div');
+
+    await page.waitForSelector('#mount_0_0_LO > div > div > div:nth-child(3) > div > div > div.x9f619.x1n2onr6.x1ja2u2z > div > div.x1uvtmcs.x4k7w5x.x1h91t0o.x1beo9mf.xaigb6o.x12ejxvf.x3igimt.xarpa2k.xedcshv.x1lytzrv.x1t2pt76.x7ja8zs.x1n2onr6.x1qrby5j.x1jfb8zj > div > div > div > div > div.x7r02ix.xf1ldfh.x131esax.xdajt7p.xxfnqb6.xb88tzc.xw2csxc.x1odjw0f.x5fp0pe > div > div > div > div.x15wfb8v.x3aagtl.x6ql1ns.x1iyjqo2.xs83m0k.xdl72j9.x13vbajr.x1ue5u6n.x78zum5 > div.x13ehr01.x9f619.x78zum5.x1n2onr6.x1f4304s > div > div > div > div.x9f619.xjbqb8w.x78zum5.x168nmei.x13lgxp2.x5pf9jr.xo71vjh.x1n2onr6.x1plvlek.xryxfnj.x1c4vz4f.x2lah0s.xdt5ytf.xqjyukv.x1qjc9v5.x1oa3qoh.x1nhvcw1 > div.x78zum5.x6s0dn4.x1n2onr6.xh8yej3 > div.xw2csxc.x1odjw0f.x1n2onr6.x1hnll1o.xpqswwc.x5dp1im.xl565be.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x1w2wdq1.xen30ot.x1swvt13.x1pi30zi.xh8yej3.x5n08af');
+    await page.type('#mount_0_0_LO > div > div > div:nth-child(3) > div > div > div.x9f619.x1n2onr6.x1ja2u2z > div > div.x1uvtmcs.x4k7w5x.x1h91t0o.x1beo9mf.xaigb6o.x12ejxvf.x3igimt.xarpa2k.xedcshv.x1lytzrv.x1t2pt76.x7ja8zs.x1n2onr6.x1qrby5j.x1jfb8zj > div > div > div > div > div.x7r02ix.xf1ldfh.x131esax.xdajt7p.xxfnqb6.xb88tzc.xw2csxc.x1odjw0f.x5fp0pe > div > div > div > div.x15wfb8v.x3aagtl.x6ql1ns.x1iyjqo2.xs83m0k.xdl72j9.x13vbajr.x1ue5u6n.x78zum5 > div.x13ehr01.x9f619.x78zum5.x1n2onr6.x1f4304s > div > div > div > div.x9f619.xjbqb8w.x78zum5.x168nmei.x13lgxp2.x5pf9jr.xo71vjh.x1n2onr6.x1plvlek.xryxfnj.x1c4vz4f.x2lah0s.xdt5ytf.xqjyukv.x1qjc9v5.x1oa3qoh.x1nhvcw1 > div.x78zum5.x6s0dn4.x1n2onr6.xh8yej3 > div.xw2csxc.x1odjw0f.x1n2onr6.x1hnll1o.xpqswwc.x5dp1im.xl565be.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x1w2wdq1.xen30ot.x1swvt13.x1pi30zi.xh8yej3.x5n08af','This is the Caption to our post',{delay:100});
+
+    await page.keyboard.press("Tab");
+    await page.keyboard.press("Tab");
+    await page.keyboard.press("Tab");
+    await page.keyboard.press("Tab");
+    await page.keyboard.press("Tab");
+    await page.keyboard.press("Enter");
+    await page.waitFor(3000);
+    
+
 
 };
 
